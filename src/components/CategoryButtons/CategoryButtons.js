216f88ba15a3be315814c2e625dd9285584cd7e5
EmmanouilM <manosgr55@hotmail.com>
2022-03-15 12:07:21 +0100

    create and style left side buttons

--- src/components/CategoryButtons/CategoryButtons.js
+++ src/components/CategoryButtons/CategoryButtons.js
@@ -3,8 +3,27 @@ import './CategoryButtons.css'
 
 const CategoryButtons = () => {
   return (
-    <div>CategoryButtons</div>
-  )
-}
+    <div className='category-buttons'>
+      <button className='button__hotel'>
+        <div className='button__content'>
+          <span className='icon icon-hotel'></span>
+          <span>Hotels</span>
+        </div>
+      </button>
+      <button>
+        <div className='button__content'>
+          <span className='icon icon-restaurant'></span>
+          <span>Restaurants</span>
+        </div>
+      </button>
+      <button>
+        <div className='button__content'>
+          <span className='icon icon-attraction'></span>
+          <span>Attractions</span>
+        </div>
+      </button>
+    </div>
+  );
+};
 
 export default CategoryButtons
