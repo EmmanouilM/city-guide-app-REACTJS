89eab6aa5213c3c7bf271fadc0dea725ea772c1e
EmmanouilM <manosgr55@hotmail.com>
2022-03-15 13:33:25 +0100

    render Google Map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,10 +1,23 @@
-import React from 'react'
-import './Map.css'
+import React from "react";
+import "./Map.css";
+import GoogleMapReact from "google-map-react";
 
 const Map = () => {
+  const coordinates = { lat: 0, lng: 0 };
   return (
-    <div>Map</div>
-  )
-}
+    <div className='map-container'>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
 
-export default Map
+export default Map;
