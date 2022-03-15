c55a0426028416ead735b05843fb252cc54f3fd3
EmmanouilM <manosgr55@hotmail.com>
2022-03-15 11:53:25 +0100

    style page- set css variables

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import './App.css'
-import Header from './components/Header/Header'
-import CategoryButtons from './components/CategoryButtons/CategoryButtons'
-import Map from './components/Map/Map'
-import PlacesList from './components/PlacesList/PlacesList'
+import "./App.css";
+import Header from "./components/Header/Header";
+import CategoryButtons from "./components/CategoryButtons/CategoryButtons";
+import Map from "./components/Map/Map";
+import PlacesList from "./components/PlacesList/PlacesList";
 
 const App = () => {
-  return <div>
-    <Header/>
-    <CategoryButtons/>
-    <Map/>
-    <PlacesList/>
-  </div>;
+  return (
+    <div className='container'>
+      <Header />
+      <div className='content'>
+        <CategoryButtons />
+        <Map />
+        <PlacesList />
+      </div>
+    </div>
+  );
 };
 
 export default App;
