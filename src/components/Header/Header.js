37a1f778a330fd543025d754bf3962a2c09a7dc6
EmmanouilM <manosgr55@hotmail.com>
2022-03-15 11:59:14 +0100

    create Header component - add style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className='header'>
+      <div className='header__title'>
+        City<span>guide</span>
+      </div>
+      <form action='#' className=' header__search'>
+        <input
+          type='text'
+          className='header__search-input'
+          placeholder='Search a new place...'
+        />
+        <button className='header__search-button'>
+          <FontAwesomeIcon
+            className='header__search-icon'
+            icon={faMagnifyingGlass}
+          />
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
